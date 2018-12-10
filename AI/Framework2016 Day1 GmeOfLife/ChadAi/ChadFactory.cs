d45c611f72dde745f2116c11b5e6e256c0f16c47
Unknown <andr4376@gmail.com>
2018-12-10 12:13:39 +0100

    MyAi Implemented

---
+++ AI/Framework2016 Day1 GmeOfLife/ChadAi/ChadFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AIFramework;
+using AIFramework.Entities;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ChadAi
+{
+    public class ChadFactory : AgentFactory
+    {
+        public override Agent CreateAgent(IPropertyStorage propertyStorage)
+        {
+            return new Chad(propertyStorage);
+        }
+
+        public override Agent CreateAgent(Agent parent1, Agent parent2, IPropertyStorage propertyStorage)
+        {
+            return new Chad(propertyStorage);
+        }
+
+        public override Type ProvidedAgentType
+        {
+            get { return typeof(Chad); }
+        }
+
+        public override string Creators
+        {
+            get { return "Andreas Jensen"; }
+        }
+
+        public override void RegisterWinners(List<Agent> sortedAfterDeathTime)
+        {
+            //Do data collection - Perhaps used to evolutionary algoritmen
+        }
+    }
+}
+
